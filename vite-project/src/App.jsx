2e0ee00ed374300e6dc4d56af771bf33331fe0f7
Bizzy-coding <bizzycoding@gmail.com>
2023-03-25 20:36:55 +0000

    more object rendering practice

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -7,6 +7,7 @@ export default function App() {
       <Profile />
       <MyButton />
       <Doggo />
+      <RecipeList />
     </div>
 
   )
@@ -16,12 +17,6 @@ function MyButton() {
   return (
     <button>Click me</button>
   )
-}
-
-const user = {
-  name: 'Miss Vanjee',
-  imageUrl: 'https://dragadventures.files.wordpress.com/2018/03/capture30.jpg',
-  imageSize: 190,
 }
 
 function Profile() {
@@ -39,24 +34,68 @@ function Profile() {
   )
 }
 
+
+function Doggo() {
+  const listToys = bettyToys.map(bettyToy =>
+    <li key={bettyToy.id}>
+      {bettyToy.name}
+    </li>
+    );
+
+  return (
+    <>
+     <h2>{dog.name}</h2>
+    <img
+      src={dog.imgLink}
+      style={{
+      width: dog.pawSize,
+      }} 
+      />
+      <ul>{listToys}</ul>
+    </>
+  );
+}
+
+function RecipeList() {
+
+  const listItems = recipes.map(recipe =>
+    <li
+    key={recipe.id}
+    style={{
+      color: recipe.isDinner ? 'red' : 'pink'
+    }}
+    >
+      {recipe.title}
+    </li>
+
+  )
+  return (
+    <ul>{listItems}</ul>
+  )
+}
+
+const user = {
+  name: 'Miss Vanjee',
+  imageUrl: 'https://dragadventures.files.wordpress.com/2018/03/capture30.jpg',
+  imageSize: 190,
+}
+
+const bettyToys = [
+  { name: 'weasel', id: 1 },
+  { name: 'mr brocoli', id: 2 },
+  { name: 'squid', id: 3 }
+];
+
 const dog = {
   name: 'Betty',
   food: 'Denta Sticks',
   imgLink: 'https://paw-dicure.com/wp-content/uploads/2021/06/icon.png',
   pawSize: 90,
-
 }
 
-function Doggo() {
-  return(
-    <>
-    <h2>{dog.name}</h2>
-    <img
-      src={dog.imgLink}
-      style={{
-        width: dog.pawSize,
-      }}
-    />
-    </>
-  )
-}
+const recipes = [
+  { title: 'spag bol', id: 1, isDinner: true },
+  { title: 'pizza', id: 2, isDinner: true },
+  { title: 'toast', id: 3, isDinner: false },
+];
+
